1e8fda3d7b1d0993eed8553d0816ff488aec2f19
Aviral <aviraltyagi.mrt@gmail.com>
2026-09-13 21:11:20 +0530

    Function created that shows fixed progress Bar

--- Progress_Bar.c
+++ Progress_Bar.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
-int main(int argc,char *argv[]){
-    
+#include <stdlib.h>
+#include <string.h>
+const int BAR_LENGTH = 80;
+void print_bar(int id, int progress)
+{
+     
+    int bars_to_show = progress * BAR_LENGTH / 100;
+    printf("Task %d = [",id);
+    for(int i = 0;i<BAR_LENGTH;i++)
+    {
+        if(i < bars_to_show)
+        {
+            printf("=");
+        }
+        else
+        {
+            printf(" ");
+        }
+    }
+    printf("] %d %%",progress);
+}
+
+int main(int argc,char *argv[])
+{
+    print_bar(1,80);
+    printf("\n");
+    print_bar(2,100);
+    printf("\n");
+    print_bar(3,85);
+    return 0;
 }

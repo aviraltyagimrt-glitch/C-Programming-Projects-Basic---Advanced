eb763e279fbc63706bd69edf6eba04106590f3b6
Aviral <aviraltyagi.mrt@gmail.com>
2026-09-15 23:02:32 +0530

    Function created for registering

--- user_management.c
+++ user_management.c
@@ -1,37 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+char function1()
+{
+    char *username;
+    username = (char *)malloc(50 * sizeof(char));
+    printf("Enter your username: -  ");
+    scanf("%s", username);
+    if (strlen(username) < 1 || strlen(username) > 8)
+    {
+        printf("Username must be at least 8 characters long.\n");
+        free(username);
+        return 0;
+    }
+    printf("Your username is %s\n", username);
+    free(username);
+    printf("Now create a password for this username : - ");
+    char *password = (char *)malloc(50 * sizeof(char));
+    scanf("%s", password);
+    if (strlen(password) < 0 || strlen(password) > 8)
+    {
+        printf("Password length must be of 8 characters only\n");
+        free(password);
+        return 0;
+    }
+}
 int main(int argc, char *argv[])
 {
+    while(true)
+    {
+        
+    }
     printf("Welcome to the User Management System\n");
     printf("Press 1 to Register a new user\n");
     printf("Press 2 to Login\n");
     printf("Press 3 to Exit\n");
     int choice;
-    char username[50];
-    char password[9];
-    scanf(" %d", &choice);
-    while (getchar() != '\n')
+    scanf("%d", &choice);
+    char entered_username[50];
+    // if (choice == 1)
+    // {
+    //     username = (char *)malloc(50 * sizeof(char));
+    //     printf("Enter your username: -  ");
+    //     scanf("%s", username);
+    //     if (strlen(username) < 1 || strlen(username) > 8)
+    //     {
+    //         printf("Username must be at least 8 characters long.\n");
+    //         free(username);
+    //         return 0;
+    //     }
+    //     printf("Your username is %s\n", username);
+    //     free(username);
+    //     printf("Now create a password for this username : - ");
+    //     char *password = (char *)malloc(50 * sizeof(char));
+    //     scanf("%s", password);
+    //     if (strlen(password) < 0 || strlen(password) > 8)
+    //     {
+    //         printf("Password length must be of 8 characters only\n");
+    //         free(password);
+    //         return 0;
+    //     }
+    // }
+    else if (choice == 2)
     {
-        if (choice == 1)
+        printf("Enter your username: -  ");
+        scanf("%s", entered_username);
+        if (strlen(entered_username) < 1 || strlen(entered_username) > 8)
         {
-            do
-            {
-                printf("Before creating your new username kindly follow these instructions\nThe username must be of 8 characters\n");
-                printf("Enter your new username :- \n");
-                gets(username);
-                printf("Username genertaed successfully\n");
-
-            }while((strlen(username) < 0 || strlen(username) > 8));
-            do
-            {
-                printf("Before you create your password , Kindly follow these instructions\nThe password must also be of 8 characters\n");
-                printf("Create your password between 1 - 8 characters :- \n");
-
-            }while((strlen(password) < 1 || strlen(password) > 8));
-            gets(password);
-            printf("User %s registered successfully with password %s \n", username, password);
+            printf("Kindly Enter the username of the length generated\n");
+            return 0;
         }
+        else if (entered_username == username)
+        {
+            printf("You logged in successfully\n");
+            return 1;
+        }
+    }
+    else if (choice == 3)
+    {
+        printf("You successfully exited the program developed by Aviral Tyagi\n");
+    }
+    else
+    {
+        printf("Invalid choice. Please try again.\n");
         return 0;
     }
+    return 0;
 }
